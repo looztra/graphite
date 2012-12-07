7c296026eda8609c2fc66b6a98a62c8b30aeebeb
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2012-12-07 21:39:29 +0100

    upgrade to graphite 0.9.10 and python 2.7
    original commit : https://github.com/scottso/cookbook-graphite/commit/f91bb7014acdd4e7d015a3a187cd6eba610f2045

--- attributes/graphite.rb
+++ attributes/graphite.rb
@@ -1,14 +1,14 @@
-default['graphite']['version'] = "0.9.9"
-default['graphite']['python_version'] = "2.6"
+default['graphite']['version'] = "0.9.10"
+default['graphite']['python_version'] = "2.7"
 
-default['graphite']['carbon']['uri'] = "http://launchpadlibrarian.net/82112362/carbon-#{node['graphite']['version']}.tar.gz"
-default['graphite']['carbon']['checksum'] = "b3d42e3b93c09"
+default['graphite']['carbon']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/carbon-#{node['graphite']['version']}.tar.gz"
+default['graphite']['carbon']['checksum'] = "4f37e00595b5b078edb9b3f5c"
 
-default['graphite']['whisper']['uri'] = "http://launchpadlibrarian.net/82112367/whisper-#{node['graphite']['version']}.tar.gz"
-default['graphite']['whisper']['checksum'] = "66c05eafe8d86"
+default['graphite']['whisper']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/whisper-#{node['graphite']['version']}.tar.gz"
+default['graphite']['whisper']['checksum'] = "36b5fa917526224678da0a530"
 
-default['graphite']['graphite_web']['uri'] = "http://launchpadlibrarian.net/82112308/graphite-web-#{node['graphite']['version']}.tar.gz"
-default['graphite']['graphite_web']['checksum'] = "cc78bab7fb26b"
+default['graphite']['graphite_web']['uri'] = "https://launchpad.net/graphite/0.9/#{node['graphite']['version']}/+download/graphite-web-#{node['graphite']['version']}.tar.gz"
+default['graphite']['graphite_web']['checksum'] = "4fd1d16cac3980fddc09dbf0a"
 
 default['graphite']['carbon']['line_receiver_interface'] =   "127.0.0.1"
 default['graphite']['carbon']['pickle_receiver_interface'] = "127.0.0.1"
